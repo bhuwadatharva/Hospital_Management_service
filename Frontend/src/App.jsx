18f4e31d99d84abc6ade6e18c61fd94e95e61320
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2024-08-21 20:45:42 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/me",
+          "https://hospital-management-service-n77u.onrender.com/api/v1/user/me",
           {
             withCredentials: true,
           }
