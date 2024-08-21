ca85a899d1c60d293ea8ecb346fac0e5a949d14a
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2024-08-21 20:47:07 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://hospital-management-service-n77u.onrender.com/api/v1/user/login",
         { email, password, role: "patient" },
         {
           withCredentials: true,
