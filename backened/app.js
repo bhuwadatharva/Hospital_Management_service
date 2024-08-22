e36ebc38da026984d7e34370251feb3d37b8c354
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2024-08-22 17:18:04 +0530

    Update app.js

--- backened/app.js
+++ backened/app.js
@@ -27,27 +27,14 @@ app.use("/files", express.static("files"));
 config({ path: "./config/config.env" });
 
 
-
 app.use((req, res, next) => {
-       res.header('Access-Control-Allow-Origin', 'https://hospital-management-service-dun.vercel.app');
-       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-       next();
-     });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
-app.use((req, res, next) => {
-       res.header('Access-Control-Allow-Origin', 'https://hospital-management-service-28ri.vercel.app');
-       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-       next();
-     });
 
-app.use((req, res, next) => {
-       res.header('Access-Control-Allow-Origin', 'https://hospital-management-service-nejv.vercel.app');
-       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-       next();
-     });
 
 app.use(cookieParser());
 app.use(express.json());
